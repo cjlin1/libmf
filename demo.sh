2b84ad53d4ef34d63d00d250dfdb42a43c390ec7
Yu-Chin <guestwalk@gmail.com>
2013-09-03 16:18:57 +0800

    Rename movielens and acknowledge GroupLens.

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./libmf convert movielens movielens.tr.bin
-./libmf convert movielens movielens.te.bin
-./libmf train --tr-rmse --obj -k 40 -p 0.05 -q 0.05 -g 0.003 -v movielens.te.bin movielens.tr.bin model
-./libmf predict movielens.te.bin model output
+./libmf convert movielens.sub movielens.sub.tr.bin
+./libmf convert movielens.sub movielens.sub.te.bin
+./libmf train --tr-rmse --obj -k 40 -p 0.05 -q 0.05 -g 0.003 -v movielens.sub.te.bin movielens.sub.tr.bin model
+./libmf predict movielens.sub.te.bin model output
