e6872b2242ff854f9ce99424a8dbd3820a631d89
Yu-Chin <guestwalk@gmail.com>
2013-09-09 16:27:04 +0800

    use synthesized data

--- demo.sh
+++ demo.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-./libmf convert movielens.sub movielens.sub.tr.bin
-./libmf convert movielens.sub movielens.sub.te.bin
-./libmf train --tr-rmse --obj -k 40 -p 0.05 -q 0.05 -g 0.003 -blk 5x7 -v movielens.sub.te.bin movielens.sub.tr.bin model
-./libmf predict movielens.sub.te.bin model output
+./libmf convert smalldata smalldata.tr.bin
+./libmf convert smalldata smalldata.te.bin
+./libmf train --tr-rmse --obj -k 40 -p 0.05 -q 0.05 -g 0.003 -blk 5x7 -v smalldata.te.bin smalldata.tr.bin model
+./libmf predict smalldata.te.bin model output
