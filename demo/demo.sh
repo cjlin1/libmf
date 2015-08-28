792d79abcc19da2eeb6afe2c657dc9d518d5961c
Wei-Sheng Chin <d01944006@csie.ntu.edu.tw>
2015-08-28 09:05:01 +0800

    1. Modify README 2. Add data sets and a script for demo

---
+++ demo/demo.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+train=../mf-train
+predict=../mf-predict
+
+##########################################################################
+# Real-valued matrix factorization
+##########################################################################
+echo "--------------------------------"
+echo "Real-valued matrix factorization"
+echo "--------------------------------"
+# In-memory training with holdout valudation
+$train -x 0 -l2 0.05 -k 100 -t 10 -p bigdata.te.txt bigdata.tr.txt model.txt
+# Do prediction and show MAE
+$predict -e 1 bigdata.te.txt model.txt output.txt
+
+##########################################################################
+# Binary matrix factorization
+##########################################################################
+echo "---------------------------"
+echo "binary matrix factorization"
+echo "---------------------------"
+# In-memory training with holdout valudation
+$train -x 5 -l2 0.01 -k 64 -p bigdata_bin.te.txt bigdata_bin.tr.txt model.txt
+# Do prediction and show accuracy
+$predict -e 6 bigdata_bin.te.txt model.txt output.txt
+
+##########################################################################
+# One-class matrix factorization
+##########################################################################
+echo "------------------------------"
+echo "one-class matrix factorization"
+echo "------------------------------"
+# In-memory training
+$train -x 10 -l2 0.01 -k 32 bigdata_one.tr.txt model.txt
+# Do prediction and show row-oriented MPR
+$predict -e 10 bigdata_one.te.txt model.txt output.txt
+# Do prediction and show row-oriented AUC
+$predict -e 12 bigdata_one.te.txt model.txt output.txt
