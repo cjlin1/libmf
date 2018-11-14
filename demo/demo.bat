ff72509e3408f7940bc9249bcca95f994cb473c4
Wei-Sheng Chin <wechi@microsoft.com>
2018-11-13 19:54:32 -0800

    Is it a workaround?

--- demo/demo.bat
+++ demo/demo.bat
@@ -6,7 +6,7 @@ SET predict=..\windows\mf-predict
 ::#########################################################################
 echo "Real-valued matrix factorization"
 :: In-memory training with holdout valudation
-%train% -f 0 -l2 0.05 -k 100 -t 10 -p real_matrix.te.txt real_matrix.tr.txt rvmf_model.txt
+%train% -f 0 -l2 0.05 -k 100 -t 1 -p real_matrix.te.txt real_matrix.tr.txt rvmf_model.txt
 :: Do prediction and show MAE
 %predict% -e 1 real_matrix.te.txt rvmf_model.txt rvmf_output.txt
 
@@ -15,7 +15,7 @@ echo "Real-valued matrix factorization"
 ::#########################################################################
 echo "binary matrix factorization"
 :: In-memory training with holdout valudation
-%train% -f 5 -l2 0.01 -k 64 -p binary_matrix.te.txt binary_matrix.tr.txt bmf_model.txt
+%train% -f 5 -l2 0.01 -k 64 -t 1 -p binary_matrix.te.txt binary_matrix.tr.txt bmf_model.txt
 :: Do prediction and show accuracy
 %predict% -e 6 binary_matrix.te.txt bmf_model.txt bmf_output.txt
 
@@ -24,7 +24,7 @@ echo "binary matrix factorization"
 ::#########################################################################
 echo "one-class matrix factorization using a stochastic gradient method"
 :: In-memory training with holdout validation
-%train% -f 10 -l2 0.01 -k 32 -p all_one_matrix.te.txt all_one_matrix.tr.txt ocmf_model.txt
+%train% -f 10 -l2 0.01 -k 32 -t 1 -p all_one_matrix.te.txt all_one_matrix.tr.txt ocmf_model.txt
 :: Do prediction and show row-oriented MPR
 %predict% -e 10 all_one_matrix.te.txt ocmf_model.txt ocmf_output.txt
 :: Do prediction and show row-oriented AUC
